a7c707f4257059a331914a41e6058afffab64230
Alfredo Oltra <alfredo.ptcf@gmail.com>
2018-07-09 18:57:32 +0200

    Creación de la base de datos

--- database/scripts/create_database.sql
+++ database/scripts/create_database.sql
@@ -12,13 +12,13 @@
 
         b) con Docker. Desde la carpeta laradock (y con los contendores arrancados):
 
-          docker exec -i MYSQLCONTAINER mysql -uroot -proot < ../API_REST_Scrabble/database/scripts/create_database.sql
+          docker exec -i MYSQLCONTAINER mysql -uroot -proot < ../database/scripts/create_database.sql
 
           donde:
             uroot es el login del administrador de MySQL.
             proot es el passowrd del administrador de MySQL.
-            MYSQLCONTAINER es el nombre del contenedor MySQL creado, por ejemplo *laradock_mysql_1*.
-                           El nombre del contenedor se puede obtener ejecutando el comando del paso 7.
+            MYSQLCONTAINER es el nombre del contenedor MySQL creado, por ejemplo laradock_mysql_1.
+                           El nombre del contenedor se puede obtener ejecutando el comando docker ps.
 
 */
  CREATE DATABASE scrabble;
