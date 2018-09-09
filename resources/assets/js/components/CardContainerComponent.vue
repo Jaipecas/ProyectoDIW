3427ded946fea4ca54e79b143f6d1ddd34a29b4d
Alfredo Oltra <alfredo.ptcf@gmail.com>
2018-09-09 11:00:34 +0200

    Se añade soporte para BladeVariableComponent

--- resources/assets/js/components/CardContainerComponent.vue
+++ resources/assets/js/components/CardContainerComponent.vue
@@ -1,28 +1,29 @@
 <template>
-        <div class="card-container">
-            <!-- Quiero repetir output-card-component. -->
-            <!-- Un hijo no puede acceder directamente a los datos del padre 
-                 Por eso es necesario enlazarlo con un bind 
-                 v-bind:card = "pcard" enlaza el valor de card de este componente, 
-                 y lo pasa como pcard -->
-            <output-card-component v-for='card in cards' :key='card.order' v-bind:pcard='card'>
+    <div class="card-container">
+        <!-- Quiero repetir output-card-component. -->
+        <!-- Un hijo no puede acceder directamente a los datos del padre 
+                Por eso es necesario enlazarlo con un bind 
+                v-bind:card = "pcard" enlaza el valor de card de este componente, 
+                y lo pasa como pcard -->
+            <output-card-component v-if="cards" v-for='card in cards' :key='card.order' v-bind:pcard='card'>
             </output-card-component>
-        </div>
+            <blade-variable-component v-if="variables" v-for='variable in variables' :key='variable.order' v-bind:pvar='variable'>
+            </blade-variable-component>
+    </div>
 </template>
 
 <script>
 import OutputCardComponent from './OutputCardComponent'
+import BladeVariableComponent from './BladeVariableComponent'
 
 export default {
     name: 'card-container-component', /* que sea siempre compuesto con - par evitar colisiones con otros tag HTMHL5 */
     props: {
-        cards: {
-            type: Array,
-            required: true,
-        }
+        cards: { type: Array, required: false },
+        variables: { type: Array, required: false }
     },
     components: {
-        OutputCardComponent
+        OutputCardComponent, BladeVariableComponent
     }
 }
 </script>
@@ -32,11 +33,11 @@ export default {
 .card-container {
     text-align: center;
     min-width: 100%;
-    min-height: 200px;
     display: flex;
     align-items: flex-start;
     overflow-x: auto;
 }
+
 </style>
 
 
