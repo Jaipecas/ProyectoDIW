6c8c4c9e0a4000d1ec384eaf4cb3ff321015cbad
alfredo <alfredo.ptcf@gmail.com>
2019-02-09 18:33:57 +0100

    Emparejamiento reto usuario concreto

--- app/Challenge.php
+++ app/Challenge.php
@@ -61,31 +61,8 @@ class Challenge extends Model
                     ->where('requesting_player', '<>', $request->first()->id)
                     ->orderBy('created_at','asc')->first();
 
-            if ($oppoChallenge) {   // he encontrado rival
-
-                // creo la partida
-                $game = new Game;
-
-                $game->language = $inputChallenge->language;
-                $game->player_1 = $request->first()->id;
-                $game->player_2 = $oppoChallenge->requesting_player;
-                $game->state = rand(0,1) == 0 ? 'turn_p1' : 'turn_p2';
-                $game->remaining_letters = $game->initalLetters();
-        
-                $opponent = User::find($oppoChallenge->requesting_player);
-                $game->player_2_letters =  $opponent->getLetters($game, 7);
-                $req = User::find($request->first()->id);
-                $game->player_1_letters = $req->getLetters($game, 7);
-
-                $game->tableboard = implode(array_fill(0, 225, ' '));
-
-                $game->save();
-
-                // emito el evento
-                event(new AcceptedChallenge($game->id, $inputChallenge->language, $request->first(), $opponent));
-                // al contrincante
-                event(new AcceptedChallenge($game->id, $inputChallenge->language, $opponent, $request->first()));
-            }
+            if ($oppoChallenge)    // he encontrado rival
+                Challenge::createGame($inputChallenge, $oppoChallenge, $request);
 
         } else if ($inputChallenge->level != "0" && $inputChallenge->opposing_player == NULL) { // nivel
             
@@ -107,7 +84,7 @@ class Challenge extends Model
             } else {
                 for ($i = intval($inputChallenge->level);$i <= 50; $i = $i+10) { array_push($levelsRequire, $i/10); }
             }
-          //var_dump($levels);
+          
           //var_dump($levelsRequire);
             $oppoChallenge = Challenge::where('language', $inputChallenge->language)
                     ->whereIn('challenges.level', $levels)
@@ -121,36 +98,64 @@ class Challenge extends Model
                     })
                     ->orderBy('created_at','asc')->first();
 
-            if ($oppoChallenge) {   // he encontrado rival
+            if ($oppoChallenge)    // he encontrado rival
+                Challenge::createGame($inputChallenge, $oppoChallenge, $request);
 
-                // creo la partida
-                $game = new Game;
+        } else {    // contra un usuario en concreto
+            unset($levels);
+            $levels = array(0, $levelRequest);
 
-                $game->language = $inputChallenge->language;
-                $game->player_1 = $request->first()->id;
-                $game->player_2 = $oppoChallenge->requesting_player;
-                $game->state = rand(0,1) == 0 ? 'turn_p1' : 'turn_p2';
-                $game->remaining_letters = $game->initalLetters();
-                
-                $opponent = User::find($oppoChallenge->requesting_player);
-                $game->player_2_letters = $opponent->getLetters($game, 7);
-                $req = User::find($request->first()->id);
-                $game->player_1_letters = $req->getLetters($game, 7);
+            for ($i = 5;$i >= $levelRequest;$i = $i-1) { array_push($levels, -1*$i); }
+            for ($i = 10;$i <= 10*$levelRequest;$i = $i+10) { array_push($levels, $i); }
 
-                $game->tableboard = implode(array_fill(0, 225, ' '));
+            $reqid = $request->first()->id;
+            $oppoChallenge = Challenge::where(function($query1) use ($reqid, $levels) {
+                    return $query1->where('opposing_player', $reqid)
+                                  ->orWhere(function($query2) {
+                                        return $query2->where('level', '0')->where('opposing_player', NULL);
+                                    })
+                                  ->orWhere(function($query3) {
+                                        return $query3->whereIn('level', $levels)->where('opposing_player', NULL);
+                                    });
+                })->where('language', $inputChallenge->language)
+                  ->where('id', '<>', $inputChallenge->id) 
+                  ->where('requesting_player', $inputChallenge->opposing_player)
+                  ->orderBy('created_at','asc')->first();
 
-                $game->save();
 
-                // emito el evento al que genera la petición
-                event(new AcceptedChallenge($game->id, $inputChallenge->language, $request->first(), $opponent));
-                // al contrincante
-                event(new AcceptedChallenge($game->id, $inputChallenge->language, $opponent, $request->first()));
-            }
-
-        } else {
-            // TODO
+            if ($oppoChallenge)    // he encontrado rival
+                Challenge::createGame($inputChallenge, $oppoChallenge, $request);  
         }
+    }
 
-    
+    /**
+     * Crea la partida a partir de los retos y manda mensajes a los 
+     * contrincantes
+     * 
+     */
+    protected static function createGame($inputChallenge, $oppoChallenge, $request) {
+            
+        // creo la partida
+        $game = new Game;
+
+        $game->language = $inputChallenge->language;
+        $game->player_1 = $request->first()->id;
+        $game->player_2 = $oppoChallenge->requesting_player;
+        $game->state = rand(0,1) == 0 ? 'turn_p1' : 'turn_p2';
+        $game->remaining_letters = $game->initalLetters();
+
+        $game->state = rand(0,1) == 0 ? 'turn_p1' : 'turn_p2';
+        $game->remaining_letters = $game->initalLetters();
+        $req = User::find($request->first()->id);
+        $game->player_1_letters = $req->getLetters($game, 7);
+
+        $game->tableboard = implode(array_fill(0, 225, ' '));
+
+        $game->save();
+
+        // emito el evento al que genera la petición
+        event(new AcceptedChallenge($game->id, $inputChallenge->language, $request->first(), $opponent));
+        // al contrincante
+        event(new AcceptedChallenge($game->id, $inputChallenge->language, $opponent, $request->first()));
     }
 }
