fd0695dbfbeea8526e8206893d6fb07eceb69c70
Jaime Perez Castellanos <jaipecas89@gmail.com>
2022-03-15 19:54:54 +0100

    Se añade coloreado del mapa cuando pasa la ruta

--- public/js/roadAnimation.js
+++ public/js/roadAnimation.js
@@ -1,17 +1,24 @@
-let elementsAnimation = Array.from(document.getElementById('path').children)
-
 function loadAnimation() {
+    let elementsAnimation = Array.from(document.getElementById('path').children)
+    let arrayCountries = Array.from(getMapCountries());
+
     if (elementsAnimation[0].classList.contains('road-animation')) {
-        removeAnimation()
+        removeAnimation(elementsAnimation, arrayCountries)
     }
-    setTimeout(addAnimation, 50);
+
+    setTimeout(() => addAnimationRoad(elementsAnimation, arrayCountries), 50);
 }
 
-function addAnimation() {
+function addAnimationRoad(elementsAnimation, arrayCountries) {
     let delay = 0.75;
     let duration = 20;
+    let countEllipse = 0;
 
     elementsAnimation.forEach(element => {
+        if (element.nodeName === 'ellipse') {
+            addAnimationCountry(arrayCountries[countEllipse], delay, duration);
+            countEllipse++;
+        }
         element.style.setProperty('animation-delay', delay + 's');
         element.style.setProperty('animation-duration', duration + 's');
         element.classList.add('road-animation');
@@ -20,13 +27,22 @@ function addAnimation() {
     });
 }
 
-function removeAnimation() {
-    elementsAnimation.forEach(element => {
-        element.classList.remove('road-animation');
-    });
+function addAnimationCountry(country, delay, duration) {
+    country.style.setProperty('animation-delay', delay + 's');
+    country.style.setProperty('animation-duration', duration + 's');
+    country.classList.add('country-animation');
 }
 
+function removeAnimation(elementsAnimation, arrayCountries) {
+    elementsAnimation.forEach(element => element.classList.remove('road-animation'));
+    arrayCountries.forEach(element => element.classList.remove('country-animation'))
+}
 
+function getMapCountries() {
+    let arrayCountriesIds = ['IT', 'FR', 'DE', 'ES', 'ES', 'GB'];
+    let countriesElements = arrayCountriesIds.map(element => document.getElementById(element));
+    return countriesElements;
+}
 
 window.onload = () => {
     const buttonStartAnimation = document.getElementById('button-road');
